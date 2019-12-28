4782396536458ba185ff62e3bb4a6063b400c878
Sergey <xxsmotur@gmail.com>
2019-12-29 02:06:50 +0500

    initialize of client

--- logrus_loki.go
+++ logrus_loki.go
@@ -2,17 +2,30 @@ package hook
 
 import (
 	"github.com/sirupsen/logrus"
+	"github.com/afiskon/promtail-client/promtail"
 )
 
 var supportedLevels = []logrus.Level{logrus.DebugLevel, logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel}
 
 type Hook struct {
-
+	client promtail.Client
 }
 // NewHook creates a new hook for Loki
 func NewHook() (*Hook, error) {
+	conf := promtail.ClientConfig{
+		PushURL:            "http://localhost:3100/api/prom/push",
+		Labels:             labels,
+		BatchWait:          5 * time.Second,
+		BatchEntriesNumber: 10000,
+		SendLevel:      promtail.INFO,
+		PrintLevel:     promtail.ERROR,
+	  }
+	loki, err = promtail.NewClientJson(conf)
+	if err != nil {
+		return nil, fmt.Errorf("unable to init promtail client: %v", err)
+	}
 	return &Hook {
-
+		client:hook,
 	}
 }
 
