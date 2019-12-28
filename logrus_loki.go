973ebc91ea68e51106b47ce2d3bb8b69dd713368
Sergey <xxsmotur@gmail.com>
2019-12-29 02:18:48 +0500

    fix typos

--- logrus_loki.go
+++ logrus_loki.go
@@ -1,8 +1,11 @@
 package hook
 
 import (
-	"github.com/sirupsen/logrus"
+	"fmt"
+	"time"
+
 	"github.com/afiskon/promtail-client/promtail"
+	"github.com/sirupsen/logrus"
 )
 
 var supportedLevels = []logrus.Level{logrus.DebugLevel, logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel}
@@ -10,23 +13,24 @@ var supportedLevels = []logrus.Level{logrus.DebugLevel, logrus.InfoLevel, logrus
 type Hook struct {
 	client promtail.Client
 }
+
 // NewHook creates a new hook for Loki
 func NewHook() (*Hook, error) {
 	conf := promtail.ClientConfig{
 		PushURL:            "http://localhost:3100/api/prom/push",
-		Labels:             labels,
+		Labels:             "{source=\"" + "test" + "\",job=\"" + "job" + "\"}",
 		BatchWait:          5 * time.Second,
 		BatchEntriesNumber: 10000,
-		SendLevel:      promtail.INFO,
-		PrintLevel:     promtail.ERROR,
-	  }
-	loki, err = promtail.NewClientJson(conf)
+		SendLevel:          promtail.INFO,
+		PrintLevel:         promtail.ERROR,
+	}
+	loki, err := promtail.NewClientJson(conf)
 	if err != nil {
 		return nil, fmt.Errorf("unable to init promtail client: %v", err)
 	}
-	return &Hook {
-		client:hook,
-	}
+	return &Hook{
+		client: loki,
+	}, nil
 }
 
 // Fire implements interface for logrus
@@ -39,4 +43,3 @@ func (hook *Hook) Fire(entry *logrus.Entry) error {
 func (hook *Hook) Levels() []logrus.Level {
 	return supportedLevels
 }
-
